1e1fdba571c406548fc0ac65588ddf8cfb5f3df7
LizaTretyakova <elizabet.tretyakova@gmail.com>
2018-01-24 02:39:05 +0300

    backup

--- students/2017/tretiakova/1/include/ast_to_bytecode_impl.h
+++ students/2017/tretiakova/1/include/ast_to_bytecode_impl.h
@@ -1,15 +1,18 @@
 #ifndef _AST_TO_BYTECODE_IMPL_H
 #define _AST_TO_BYTECODE_IMPL_H
 
-#include "../include/mathvm.h"
 #include "../include/ast.h"
 #include "../include/mathvm.h"
 #include "../include/visitors.h"
 
 #include <vector>
 #include <stack>
+#include <map>
+#include <string>
 
 namespace mathvm {
+
+using namespace std;
 
 typedef union {
     double _doubleValue;
@@ -18,8 +21,23 @@ typedef union {
 } Value;
 
 class BytecodeTranslateVisitor : public AstBaseVisitor {
-    Bytecode bytecode; // output Instruction stream
+    typedef map<Scope*, uint16_t> ScopeMap;
+    typedef map<string, uint16_t> VarNameMap;
+
+    BytecodeFunction translated_function;
+    Code translated_code;
     stack<VarType> type_stack;
+    ScopeMap scope_map;
+    map<Scope*, VarNameMap> var_map;
+    vector<vector<AstVar*>> var_by_scope;
+
+    VarType update_type_stack_un() { // check
+        if(type_stack.size() < 1) {
+            type_stack.push(VT_INVALID);
+            return VT_INVALID;
+        }
+        return type_stack.top();
+    }
 
     VarType update_type_stack() {
         if(type_stack.size() < 2) {
@@ -33,22 +51,28 @@ class BytecodeTranslateVisitor : public AstBaseVisitor {
         VarType result;
 
         switch(t_left) {
-        case VT_STRING:
-            if(t_right == VT_STRING) {
-                result = VT_STRING;
-            } else {
-                result = VT_INVALID;
-            }
-            break;
+//        case VT_STRING:
+//            if(t_right == VT_STRING) {
+//                result = VT_STRING;
+//            } else {
+//                result = VT_INVALID;
+//            }
+//            break;
+//        case VT_DOUBLE:
+//        case VT_INT:
+//            if(t_right == VT_DOUBLE) {
+//                result = VT_DOUBLE;
+//            } else if(t_right == VT_INT) {
+//                result = t_left;
+//            } else {
+//                result = VT_INVALID;
+//            }
+//            break;
         case VT_DOUBLE:
         case VT_INT:
-            if(t_right == VT_DOUBLE) {
-                result = VT_DOUBLE;
-            } else if(t_right == VT_INT) {
-                result = t_left;
-            } else {
-                result = VT_INVALID;
-            }
+        case VT_STRING:
+            result =
+                t_right == t_left ? t_left : VT_INVALID;
             break;
         default:
             result = VT_INVALID;
@@ -67,10 +91,10 @@ class BytecodeTranslateVisitor : public AstBaseVisitor {
     void push_numeric(VarType type, Instruction i_bc, Instruction d_bc) {
         switch(type) {
         case VT_INT:
-            bytecode.add(i_bc);
+            translated_function.bytecode()->addInsn(i_bc);
             break;
         case VT_DOUBLE:
-            bytecode.add(d_bc);
+            translated_function.bytecode()->addInsn(d_bc);
             break;
         default:
             invalidate();
@@ -81,13 +105,13 @@ class BytecodeTranslateVisitor : public AstBaseVisitor {
     void push_comparison(VarType type, Instruction i_bc, Instruction d_bc, Instruction s_bc) {
         switch(type) {
         case VT_INT:
-            bytecode.add(i_bc);
+            translated_function.bytecode()->addInsn(i_bc);
             break;
         case VT_DOUBLE:
-            bytecode.add(d_bc);
+            translated_function.bytecode()->addInsn(d_bc);
             break;
         case VT_STRING:
-            bytecode.add(s_bc);
+            translated_function.bytecode()->addInsn(s_bc);
             break;
         default:
             invalidate();
@@ -100,7 +124,76 @@ class BytecodeTranslateVisitor : public AstBaseVisitor {
             invalidate();
             break;
         }
-        bytecode.add(bc);
+        translated_function.bytecode()->addInsn(bc);
+    }
+
+    void push_store(VarType type) {
+        VarType last_type = update_type_stack_un();
+        if(last_type != type) {
+            invalidate();
+            return;
+        }
+        switch(type) {
+        case VT_INT:
+            translated_function.bytecode()->addInsn(BC_STORECTXIVAR);
+            break;
+        case VT_DOUBLE:
+            translated_function.bytecode()->addInsn(BC_STORECTXDVAR);
+            break;
+        case VT_STRING:
+            translated_function.bytecode()->addInsn(BC_STORECTXSVAR);
+            break;
+        default:
+            invalidate();
+            break;
+        }
+        return;
+    }
+
+    // returns the position of the jmp's argument, 0 on error
+    uint32_t push_jump() {
+        if(type_stack.size() < 2) {
+            invalidate();
+            return 0;
+        }
+
+        VatType type_zero = type_stack.pop();
+        VarType type_expr = type_stack.pop();
+        if(type_zero == VT_INT && type_expr == VT_INT) {
+            // if `cond == 0`, i.e. false -- jump to "after `then`"
+            translated_function.bytecode()->addInsn(BC_IFICMPE);
+            uint32_t index = translated_function.bytecode()->current();
+            translated_function.bytecode()->addTyped<int16_t>(0); // temporarily
+            return index;
+        }
+
+        invalidate();
+        return 0;
+    }
+
+    uint16_t store_scope() {
+        if(!scope_map.count(scope)) {
+            uint16_t scope_id = (uint16_t)scope_map.size();
+            scope_map.insert(scope, scope_id);
+            var_map.insert(scope, VarNameMap());
+            var_by_scope.push_back(vector<AstVar*>());
+            return scope_id;
+        }
+        return scope_map[scope];
+    }
+
+    uint16_t store_var(Scope* scope, AstVar* var) {
+        string name = var->name();
+        uint16_t scope_id = store_scope();
+        VarNameMap smap = var_map[scope];
+
+        if(!smap.count(name)) {
+            uint16_t var_id = smap.size();
+            smap.insert(name, var_id);
+            var_by_scope[scope_id].push_back(var);
+            return var_id;
+        }
+        return smap[name];
     }
 
 public:
@@ -142,10 +235,10 @@ public:
         case tLE:
             if(type == VT_INT || type == VT_STRING) {
                 // let's say that strings are only equal if they are the same
-                bytecode.add(BC_ICMP);
-            } else {
-                bytecode.add(BC_DCMP);
+                bytecode.addInsn(BC_ICMP);
+                break;
             }
+            bytecode.add(BC_DCMP);
             break;
         case tADD:
             push_numeric(type, BC_IADD, BC_DADD);
@@ -162,9 +255,9 @@ public:
         case tMOD:
             if(type == VT_INT) {
                 bytecode.add(BC_MOD);
-            } else {
-                invalidate();
+                break;
             }
+            invalidate();
             break;
         }
     }
@@ -174,7 +267,7 @@ public:
 
         node->operand()->visit(this);
 
-        VarType type = type_stack.top();
+        VarType type = update_type_stack_un();
         if(type == VT_INVALID || type == VT_VOID || type == VT_STRING) {
             invalidate();
             return;
@@ -187,7 +280,7 @@ public:
                 break;
             }
             bytecode.addTyped<uint16_t>(0);
-            bytecode.add(BC_ICMP);
+            bytecode.addInsn(BC_ICMP);
             break;
         case tSUB:
             push_numeric(type, BC_INEG, BC_DNEG);
@@ -201,43 +294,98 @@ public:
     virtual void visitStringLiteralNode(StringLiteralNode* node) {
         cerr << "[StringLiteral]" << endl;
 
-        string literal = node->literal();
-        string new_literal = "";
-        for(int i = 0; i < (int)literal.size(); ++i) {
-            if(literal[i] != '\n') {
-                new_literal += literal[i];
-            } else {
-                new_literal += "\\n";
-            }
-        }
-        pout << "'" << new_literal << "'";
+        uint16_t const_id = translated_code.makeStringConstant(node->literal());
+        translated_function.bytecode()->addInsn(BC_SLOAD);
+        translated_function.bytecode()->addTyped(const_id);
     }
 
     virtual void visitDoubleLiteralNode(DoubleLiteralNode* node) {
         cerr << "[DoubleLiteral]" << endl;
 
-        pout << node->literal();
+        translated_function.bytecode()->addInsn(BC_DLOAD);
+        translated_function.bytecode()->addDouble(node->literal());
     }
 
     virtual void visitIntLiteralNode(IntLiteralNode* node) {
         cerr << "[IntLiteral]" << endl;
 
-        pout << node->literal();
+        translated_function.bytecode()->addInsn(BC_ILOAD);
+        translated_function.bytecode()->addInt64(node->literal());
     }
 
     virtual void visitLoadNode(LoadNode* node) {
         cerr << "[Load]" << endl;
 
         const AstVar* var = node->var();
-        pout << var->name();
+        Scope* scope = var->owner();
+        uint16_t scope_id = store_scope(scope);
+        uint16_t var_id = store_var(scope, var);
+
+        VarType type = var->type();
+        switch(type) {
+        case VT_INT:
+            translated_function.bytecode()->addInsn(BC_LOADCTXIVAR);
+            break;
+        case VT_DOUBLE:
+            translated_function.bytecode()->addInsn(BC_LOADCTXDVAR);
+            break;
+        case VT_STRING:
+            translated_function.bytecode()->addInsn(BC_LOADCTXSVAR);
+            break;
+        default:
+            invalidate();
+            break;
+        }
+
+        translated_function.bytecode()->addTyped(scope_id);
+        translated_function.bytecode()->addTyped(var_id);
     }
 
     virtual void visitStoreNode(StoreNode* node) {
         cerr << "[Store]" << endl;
 
-        pout << node->var()->name() << " "
-                  << tokenOp(node->op()) << " ";
+//        pout << node->var()->name() << " "
+//                  << tokenOp(node->op()) << " ";
+//        node->value()->visit(this);
+
+        TokenKind op = node->op();
+        AstVar* var = node->var();
+        Scope* scope = var->owner();
+        VarType type = var->type();
+
+        uint16_t scope_id = store_scope(scope);
+        uint16_t var_id = store_var(scope, var);
+
+        if(op == tINCRSET || op == tDECRSET) {
+            switch(type) {
+            case VT_INT:
+                translated_function.bytecode()->addInsn(BC_LOADCTXIVAR);
+                type_stack.push(VT_INT);
+                break;
+            case VT_DOUBLE:
+                translated_function.bytecode()->addInsn(BC_LOADCTXDVAR);
+                type_stack.push(VT_DOUBLE);
+                break;
+            default:
+                invalidate();
+                break;
+            }
+
+            translated_function.bytecode()->addTyped(scope_id);
+            translated_function.bytecode()->addTyped(var_id);
+        }
+
         node->value()->visit(this);
+
+        if(op == tINCRSET) {
+            push_numeric(type, BC_IADD, BC_DADD);
+        } else if(op == tDECRSET) {
+            push_numeric(type, BC_ISUB, BC_DSUB);
+        }
+
+        push_store(type);
+        translated_function.bytecode()->addTyped(scope_id);
+        translated_function.bytecode()->addTyped(var_id);
     }
 
     virtual void visitForNode(ForNode* node) {
@@ -265,20 +413,25 @@ public:
     virtual void visitIfNode(IfNode* node) {
         cerr << "[If]" << endl;
 
-        pout << "if (";
+//        pout << "if (";
+//        node->ifExpr()->visit(this);
+//        pout << ") {\n";
+//        node->thenBlock()->visit(this);
+//        cerr << "[IF indent_size " << indent_size << "]" << endl;
+//        pout << indent << "}";
+//        BlockNode* elseBlock = node->elseBlock();
+//        if (elseBlock) {
+//            pout << " else {";
+//            elseBlock->visit(this);
+//            pout << indent << "}";
+//        }
+//        pout << "\n";
+//        need_semicolon = false;
+
         node->ifExpr()->visit(this);
-        pout << ") {\n";
-        node->thenBlock()->visit(this);
-        cerr << "[IF indent_size " << indent_size << "]" << endl;
-        pout << indent << "}";
-        BlockNode* elseBlock = node->elseBlock();
-        if (elseBlock) {
-            pout << " else {";
-            elseBlock->visit(this);
-            pout << indent << "}";
-        }
-        pout << "\n";
-        need_semicolon = false;
+        translated_function.bytecode()->addInsn(BC_ILOAD0);
+        push_jump(...);
+        translated_function.bytecode()->addInsn();
     }
 
     virtual void visitBlockNode(BlockNode* node) {
